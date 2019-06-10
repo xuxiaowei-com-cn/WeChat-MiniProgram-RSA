5b951f66cdc101dd110ab21057619ca8984ce9c2
徐晓伟 <xuxiaowei@live.hk>
2019-06-10 18:51:46 +0800

    新建项目提交。

---
+++ java/src/main/java/cn/com/xuxiaowei/WeChatMiniProgramRsaApplication.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ */
+@SpringBootApplication
+public class WeChatMiniProgramRsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatMiniProgramRsaApplication.class, args);
+    }
+
+}
