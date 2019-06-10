d429ab94a65d0779e9c00ffac0b83a237f1312af
徐晓伟 <xuxiaowei@live.hk>
2019-06-10 20:50:22 +0800

    解密

--- java/src/main/java/cn/com/xuxiaowei/controller/RsaRestController.java
+++ java/src/main/java/cn/com/xuxiaowei/controller/RsaRestController.java
@@ -1,5 +1,6 @@
 package cn.com.xuxiaowei.controller;
 
+import cn.com.xuxiaowei.util.rsa.RsaUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.security.GeneralSecurityException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,8 +40,57 @@ public class RsaRestController {
             map.put("msg", "session 中无 publicKey");
         } else {
             map.put("code", 0);
-            map.put("msg", "成功获取到 session 中的 publicKey");
+            map.put("msg", "成功获取到 session 中的 RSA publicKey");
             data.put("publicKey", publicKey);
+        }
+
+        return map;
+    }
+
+    /**
+     * 解密
+     *
+     * @param text 需要解密的数据
+     */
+    @RequestMapping("/decrypt.do")
+    public Map<String, Object> decrypt(HttpServletRequest request, HttpServletResponse response, String text) {
+
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
+        HttpSession session = request.getSession();
+
+        String privateKey = (String) session.getAttribute("privateKey");
+
+        if (StringUtils.isEmpty(privateKey)) {
+            map.put("code", 0);
+            map.put("msg", "session 中无 privateKey");
+        } else {
+
+            System.err.println("解密前的字符串为："+ text);
+
+            try {
+
+                String decryptText = new String(RsaUtils.decryptByPrivateKey(text, privateKey));
+
+                System.err.println("解密后的字符串为："+ decryptText);
+
+                map.put("code", 0);
+                map.put("msg", "成功解密");
+
+                data.put("text", decryptText);
+
+            } catch (GeneralSecurityException e) {
+
+                map.put("code", 1);
+                map.put("msg", "成功失败");
+
+                return map;
+
+                // <code>e.printStackTrace();</code>
+            }
+
         }
 
         return map;
